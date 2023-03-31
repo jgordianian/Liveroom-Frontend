4e2a3556dc578735899601d0f4970ca6008159a2
Jonah Gordian <61239606+jgordianian@users.noreply.github.com>
2023-03-31 11:55:58 -0400

    Initial customizations done.

--- src/util/Config.js
+++ src/util/Config.js
@@ -1,2 +1,2 @@
-exports.baseURL = "https://zorona.ddns.net/"; // YOUR_BASE_URL   //same as backend
-exports.key = "sec261"; // YOUR_SECRET_KEY  // same as backend;
+exports.baseURL = "https://backzorona.ddns.net/"; // YOUR_BASE_URL   //same as backend
+exports.key = "security261"; // YOUR_SECRET_KEY  // same as backend;
