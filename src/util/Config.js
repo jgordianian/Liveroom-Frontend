8d77d6401e129f840592a98d8746a80188e60fe2
Jonah Gordian <61239606+jgordianian@users.noreply.github.com>
2023-11-25 23:14:59 -0400

    General upgrades

--- src/util/Config.js
+++ src/util/Config.js
@@ -1,2 +1,2 @@
 exports.baseURL = "https://back.zoronaliverooms.com:5000/"; // YOUR_BASE_URL   //same as backend
-exports.key = "security261"; // YOUR_SECRET_KEY  // same as backend;
+exports.key = "securityMaster"; // YOUR_SECRET_KEY  // same as backend;
