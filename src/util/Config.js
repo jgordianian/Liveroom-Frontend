10dd363853f3389506fbcf9718a685bf11026489
Jonah Gordian <61239606+jgordianian@users.noreply.github.com>
2024-02-08 11:25:11 -0400

    Update config for LOCALHOST

--- src/util/Config.js
+++ src/util/Config.js
@@ -1,2 +1,2 @@
-exports.baseURL = "https://127.0.0.1:5000/"; // YOUR_BASE_URL   //same as backend
+exports.baseURL = "http://127.0.0.1:5555/"; // YOUR_BASE_URL   //same as backend
 exports.key = "securityMaster"; // YOUR_SECRET_KEY  // same as backend;
